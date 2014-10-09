76125e817eb61bd6d3d7077d3f9585643bdf0cb7
searchivarius <leo@boytsov.info>
2014-10-08 22:05:34 -0400

    solving the leading slash issue

--- read_resource_example/dict-user/src/main/java/test.java
+++ read_resource_example/dict-user/src/main/java/test.java
@@ -1,10 +1,22 @@
 import java.io.*;
 public class test {
   public static void main(String args[]) throws Exception {
+    // Without the forward slash
     InputStream is = 
 	    test.class.getClassLoader().getResourceAsStream("dict.txt");
     BufferedReader br = new BufferedReader(new InputStreamReader(is, "utf-8"));
+    System.out.println("Reading the first time:");
     String ln;
+    while ((ln = br.readLine()) != null) {
+      System.out.println(ln);
+    }
+
+    br.close();
+    System.out.println("Reading the second time:");
+    // With the forward slash
+    is = 
+	    test.class.getResourceAsStream("/dict.txt");
+    br = new BufferedReader(new InputStreamReader(is, "utf-8"));
     while ((ln = br.readLine()) != null) {
       System.out.println(ln);
     }
